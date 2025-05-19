ff34802c1934b91e848d7d0ace692f676729af36
Your Name <supriyoghorui645@gmail.com>
2025-05-20 01:35:03 +0530

    mini change

---
+++ index.js
@@ -0,0 +1,82 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const methodoverride=require("method-override")
+const Chat = require("./Model/chat"); 
+const app = express();
+const port = 8000;
+
+// EJS setup
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname,"public")));
+app.use(methodoverride("_method"))
+
+//mongodb
+
+mongoose.connect("mongodb://127.0.0.1:27017/whatsapp", {
+}).then(() => {
+    console.log("MongoDB connected");
+}).catch((err) => {
+    console.error("MongoDB connection error:", err);
+});
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.get("/", (req, res) => {
+    res.send("server is working");
+});
+
+//index route
+app.get("/chats", async (req, res) => {
+    const chats = await Chat.find();
+    res.render("index.ejs", { chats });
+});
+
+app.get("/chats/new", async (req, res) => {
+    res.render("new.ejs")
+});
+
+app.post("/chats",(req,res)=>{
+    let {from,to,msg}=req.body
+    const newchat= new Chat({
+        from:from,
+        to:to,
+        msg:msg,
+        created_at:new Date()
+    })
+    newchat.save().then(
+        ()=>console.log("chat is saved")
+    ).catch(e=>console.log(e))
+    res.redirect("/chats");
+})
+
+app.get("/chats/:id/edit",async (req,res)=>{
+    let {id}=req.params
+    let chat=await Chat.findById(id)
+    res.render("edit.ejs",{chat})
+})
+app.put("/chats/:id",async (req,res)=>{
+  let {id}=req.params
+    let {msg : newmsg}=req.body
+    console.log(newmsg)
+    let updatedchat = await Chat.findByIdAndUpdate(
+        id,
+        {msg:newmsg},
+        {runValidators:true},{new:true}
+    )
+    console.log(updatedchat)
+    res.redirect("/chats")
+})
+app.delete("/chats/:id", async (req,res)=>{
+    let {id}=req.params
+    let deletedchat= await Chat.findByIdAndDelete(id)
+    console.log(deletedchat)
+    res.redirect("/chats")
+})
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
---
+++ init.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+const Chat = require("./Model/chat"); 
+
+mongoose.connect("mongodb://127.0.0.1:27017/whatsapp", {
+}).then(() => {
+    console.log("MongoDB connected");
+}).catch((err) => {
+    console.error("MongoDB connection error:", err);
+});
+
+const sampleChats = [
+    {
+        from: "raj",
+        to: "rahul",
+        msg: "send me photo",
+        created_at: new Date()
+    },
+    {
+        from: "priya",
+        to: "raj",
+        msg: "meeting at 5?",
+        created_at: new Date()
+    },
+    {
+        from: "amit",
+        to: "rahul",
+        msg: "let’s play PUBG",
+        created_at: new Date()
+    },
+    {
+        from: "raj",
+        to: "priya",
+        msg: "sure, I’ll be there",
+        created_at: new Date()
+    }
+];
+
+// Insert into MongoDB
+Chat.insertMany(sampleChats)
+    .then(() => {
+        console.log("Chats inserted successfully");
+    })
+    .catch((err) => {
+        console.error("Error inserting chats:", err);
+    });
